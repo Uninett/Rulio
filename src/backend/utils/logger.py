df3e3744bb3f7ca2d6165112c0a59c39018863f8
Ari Maman <ari.maman@sikt.no>
2026-06-09 13:39:39 +0200

    Initial commit, boilerplate code made, docker container for testing almost complete

--- src/backend/utils/logger.py
+++ src/backend/utils/logger.py
@@ -1,12 +1,13 @@
 import logging
 
-def setup_logger(name: str, level=logging.INFO) -> logging.Logger:
+def setup_logger(name: str, level=logging.INFO, file=None) -> logging.Logger:
     """
     Sets up a logger with the specified name and logging level.
 
     Args:
         name (str): The name of the logger.
         level: The logging level (default is logging.INFO).
+        save_to_file (str, optional): If provided, the logger will also save logs to the specified file.
 
     Returns:
         logging.Logger: Configured logger instance.
@@ -15,15 +16,24 @@ def setup_logger(name: str, level=logging.INFO) -> logging.Logger:
     logger.setLevel(level)
 
     # Create console handler and set level
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
+    channel_handler = logging.StreamHandler(stream=sys.stdout)
+    logger.addHandler(channel_handler)
+    channel_handler.setLevel(level)
+
+    # If file is provided: create file handler and set level 
+    if file is not None:
+        file_handler = logging.FileHandler(file)
+        file_handler.setLevel(level)
+        logger.addHandler(file_handler)
 
     # Create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
+    channel_handler.setFormatter(formatter)
 
     # Add the handlers to the logger
     if not logger.hasHandlers():
-        logger.addHandler(ch)
+        logger.addHandler(channel_handler)
+    
+    if 
 
     return logger
