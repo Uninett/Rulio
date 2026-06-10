35ec81660597389f34946484e5e982bf3e21aed3
Noah Johannes Borch <noah.borch.1@gmail.com>
2026-06-10 09:32:17 +0200

    Litt på create

--- src/backend/services/create.py
+++ src/backend/services/create.py
@@ -1,4 +1,6 @@
 
+import ipaddress
+
 from backend.objects import *
 
 #Temp solution for ID generation
@@ -11,24 +13,25 @@ def get_next_id() -> int:
     current_id = next_id
     return next_id
 
-def get_current_tenant_id(request) -> int:
+def get_current_tenant_id(request: object) -> int:
     if hasattr(request, 'current_tenant_id'):
         return request.current_tenant_id
     else:
         raise Exception("Tenant ID not set in request. Please set tenant using /set_tenant endpoint before creating objects.")
 
-def create_address(request, name: str, description: str, ipv4Address: str, ipv6Address: str, type: str) -> Address:
+def create_address(request: object, name: str, description: str, ipv4Address: str, ipv6Address: str, type: str) -> Address:
     id = get_next_id()
     tenant_id = get_current_tenant_id(request)
+    ipv4_addr = ipaddress.IPv4Address(ipv4Address)
+    ipv6_addr = ipaddress.IPv6Address(ipv6Address)
     address = Address(
         id=id,
         name=name,
         description=description,
         tenant_id=tenant_id,
         type=type,
-        ipv4Address=ipv4Address,
-        ipv6Address=ipv6Address,
-        type=type
+        ipv4_value=ipv4_addr,
+        ipv6_value=ipv6_addr
     )
     # Save the address to the database here
     return address
