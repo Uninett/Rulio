02710fb6a1e8bce003930602c53285727da12bdb
Selma Barmen Stein <selmabarmen@gmail.com>
2026-06-18 14:31:30 +0200

    renaming and moving functions for easier cooperation between frontend and backend

---
+++ src/backend/views.py
@@ -0,0 +1,95 @@
+# Frontend
+from django.shortcuts import render
+
+"""
+Frontend
+"""
+
+
+def get_devices_page(request):
+    return render(
+        request,
+        "devices.html",
+        {
+            "active_page": "devices",
+        },
+    )
+
+
+def get_filters_page(request):
+    return render(
+        request,
+        "filters.html",
+        {
+            "active_page": "filters",
+        },
+    )
+
+
+# This is a temporary implementation with hardcoded data for demonstration purposes.
+def get_objects_page(request):
+    addresses = [
+        {
+            "device_id": "ntnu-dns-1",
+            "description": "Primary DNS server",
+            "type": "host",
+            "name": "ntnu-dns-1",
+            "group": "Allow DNS",
+            "referenced_to": "ntnu-trd-core-r1",
+            "addresses": "ntnu-dns-1",
+            "tags": "admin",
+        },
+        {
+            "device_id": "ntnu-dns-2",
+            "description": "Secondary DNS server",
+            "type": "host",
+            "name": "ntnu-dns-2",
+            "group": "Allow DNS",
+            "referenced_to": "ntnu-trd-core-r2",
+            "addresses": "ntnu-dns-2",
+            "tags": "admin",
+        },
+    ]
+    return render(
+        request,
+        "objects.html",
+        {
+            "active_page": "objects",
+            "addresses": addresses,
+        },
+    )
+
+
+def get_empty_address_row_partial(request):
+    return render(request, "partials/objects/_addressesRowEdit.html")
+
+
+def post_address_row_partial(request):
+    address = {
+        "name": request.POST.get("name", ""),
+        "description": request.POST.get("description", ""),
+        "type": request.POST.get("type", ""),
+        "group": request.POST.get("group", ""),
+        "referenced_to": request.POST.get("referenced_to", ""),
+        "addresses": request.POST.get("addresses", ""),
+        "tags": request.POST.get("tags", ""),
+    }
+    return render(request, "partials/objects/_addressesRow.html", {"address": address})
+
+
+def get_addresses_content(request):
+    return render(request, "partials/objects/_objects_addresses.html")
+
+
+def get_services_content(request):
+    return render(request, "partials/objects/_objects_services.html")
+
+
+def get_tags_page(request):
+    return render(
+        request,
+        "tags.html",
+        {
+            "active_page": "tags",
+        },
+    )
