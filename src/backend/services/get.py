91d600d7ce69db369b01015d968f7bb61f9721ad
Ari Maman <ari.maman@sikt.no>
2026-06-18 13:55:29 +0200

    Integrate previous changes done to the address and service objects, and also allow for get endpoint for all addresses/services for one person

--- src/backend/services/get.py
+++ src/backend/services/get.py
@@ -1,4 +1,5 @@
 from ipaddress import IPv4Address, IPv6Address, IPv4Network, IPv6Network
+from webbrowser import get
 from django.core.exceptions import ValidationError as DjangoValidationError
 
 from backend.objects.attributes.address import Address
@@ -21,9 +22,7 @@ def get_all_service_groups_from_tenant(tenant_id: int) -> list[ServiceGroup]:
     return requested_service_groups
 
 
-def get_service_groups_with_services_from_tenant(
-    tenant_id: int, get="all"
-) -> list[dict]:
+def get_service_groups_with_services_from_tenant(tenant_id: int, get="all") -> list[dict]:
     service_groups = ServiceGroup.objects.filter(tenant_id=tenant_id)
 
     result = []
@@ -61,9 +60,11 @@ def get_service_groups_with_services_from_tenant(
         return result
     elif get == "ids":
         return [{"service_group_id": group["service_group_id"]} for group in result]
+    elif get == "names":
+        return [{"service_group_name": group["service_group_name"]} for group in result]
 
 
-def get_address_groups_with_addresses_from_tenant(tenant_id: int) -> list[dict]:
+def get_address_groups_with_addresses_from_tenant(tenant_id: int, get="all") -> list[dict]:
     address_groups = AddressGroup.objects.filter(tenant_id=tenant_id)
 
     result = []
@@ -102,9 +103,34 @@ def get_address_groups_with_addresses_from_tenant(tenant_id: int) -> list[dict]:
                 }
             )
 
-    return result
+    if get == "all":
+        return result
+    elif get == "ids":
+        return [{"address_group_id": group["address_group_id"]} for group in result]
+    elif get == "names":
+        return [{"address_group_name": group["address_group_name"]} for group in result]
 
 
 def get_all_address_groups_from_tenant(tenant_id: int) -> list[AddressGroup]:
     requested_address_groups = AddressGroup.objects.filter(tenant_id=tenant_id)
     return requested_address_groups
+
+
+def get_all_services_from_tenant(tenant_id: int, get="all") -> list[Service]:
+    requested_services = Service.objects.filter(tenant_id=tenant_id)
+    if get == "all":
+        return requested_services
+    elif get == "ids":
+        return [{"service_id": service.id} for service in requested_services]
+    elif get == "names":
+        return [{"service_name": service.name} for service in requested_services]
+
+
+def get_all_addresses_from_tenant(tenant_id: int, get="all") -> list[Address]:
+    requested_addresses = Address.objects.filter(tenant_id=tenant_id)
+    if get == "all":
+        return requested_addresses
+    elif get == "ids":
+        return [{"address_id": address.id} for address in requested_addresses]
+    elif get == "names":
+        return [{"address_name": address.name} for address in requested_addresses]
