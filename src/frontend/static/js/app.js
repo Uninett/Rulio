511d7c7240ac1bf2e1e2a5b8aa45eea38853e19d
Birgitte Vik <birgittevik84@gmail.com>
2026-06-22 10:01:31 +0200

    Moved logic to its own js

---
+++ src/frontend/static/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function (event) {
+    const button = event.target.closest(".btn-toggle");
+    if (!button) return;
+
+    const container = button.closest(".tools-toggle");
+    if (!container) return;
+
+    container.querySelectorAll(".btn-toggle").forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+});
