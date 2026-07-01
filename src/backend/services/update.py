ff9df462ae7f6369749cbf9712d313355fa3145f
Noah Johannes Borch <noah.borch.1@gmail.com>
2026-07-01 10:13:33 +0200

    Added more update functions

--- src/backend/services/update.py
+++ src/backend/services/update.py
@@ -1,23 +1,34 @@
-
-
-
-# Create update.py in /services for objects page (first addresses/services, then all other objects later such as devices, filters etc.). Make it possible to update/edit an existing object without creating a new object.
-
-# For object page, these methods are needed. The modal should treat the whole update as one object (i.e. address with address group), so the most important methods are update_address/service_and_groups() and update_address/service_group_and_addresses/services().
-
-
-
-
 from backend.objects.attributes.address import Address
 from backend.objects.attributes.address_group import AddressGroup
 from backend.objects.attributes.service import Service
 from backend.objects.attributes.service_group import ServiceGroup
+from backend.objects.attributes.tag import Tag
+from backend.objects.filters.filter import Filter
+from backend.objects.filters.rule import Rule
 from backend.objects.tenant_objects.device import Device
 from backend.objects.tenant_objects.device_group import DeviceGroup
+from backend.objects.tenant_objects.filter_interface import FilterInterface
+from backend.objects.tenant_objects.interface import Interface
 from backend.services.helper_user_tenant import require_write_tenant
 
 
-def update_address(*, actor, tenant_id, address_id, name=None, description=None, addr_type=None, ipv4_type=None, ipv6_type=None, ipv4Network=None, ipv6Network=None, ipv4Address_start=None, ipv4Address_end=None, ipv6Address_start=None, ipv6Address_end=None):
+def update_address(
+    *,
+    actor,
+    tenant_id,
+    address_id,
+    name=None,
+    description=None,
+    addr_type=None,
+    ipv4_type=None,
+    ipv6_type=None,
+    ipv4Network=None,
+    ipv6Network=None,
+    ipv4Address_start=None,
+    ipv4Address_end=None,
+    ipv6Address_start=None,
+    ipv6Address_end=None,
+):
     require_write_tenant(actor, tenant_id)
     address = Address.objects.get(id=address_id, tenant_id=tenant_id)
     if name is not None:
@@ -45,7 +56,19 @@ def update_address(*, actor, tenant_id, address_id, name=None, description=None,
     address.save()
     return address
 
-def update_service(*, actor, tenant_id, service_id, name=None, description=None, protocol=None, port_start=None, port_end=None, service_type=None):
+
+def update_service(
+    *,
+    actor,
+    tenant_id,
+    service_id,
+    name=None,
+    description=None,
+    protocol=None,
+    port_start=None,
+    port_end=None,
+    service_type=None,
+):
     require_write_tenant(actor, tenant_id)
     service = Service.objects.get(id=service_id, tenant_id=tenant_id)
     if name is not None:
@@ -63,6 +86,7 @@ def update_service(*, actor, tenant_id, service_id, name=None, description=None,
     service.save()
     return service
 
+
 def update_address_group(*, actor, tenant_id, address_group_id, name=None, description=None, addr_type=None):
     require_write_tenant(actor, tenant_id)
     address_group = AddressGroup.objects.get(id=address_group_id, tenant_id=tenant_id)
@@ -75,6 +99,7 @@ def update_address_group(*, actor, tenant_id, address_group_id, name=None, descr
     address_group.save()
     return address_group
 
+
 def update_service_group(*, actor, tenant_id, service_group_id, name=None, description=None, service_type=None):
     require_write_tenant(actor, tenant_id)
     service_group = ServiceGroup.objects.get(id=service_group_id, tenant_id=tenant_id)
@@ -86,6 +111,7 @@ def update_service_group(*, actor, tenant_id, service_group_id, name=None, descr
         raise ValueError("Service group type must be 'group'.")
     service_group.save()
     return service_group
+
 
 def update_device(*, actor, tenant_id, device_id, name=None, description=None, platform=None, type=None):
     require_write_tenant(actor, tenant_id)
@@ -101,6 +127,7 @@ def update_device(*, actor, tenant_id, device_id, name=None, description=None, p
     device.save()
     return device
 
+
 def update_device_group(*, actor, tenant_id, device_group_id, name=None, description=None):
     require_write_tenant(actor, tenant_id)
     device_group = DeviceGroup.objects.get(id=device_group_id, tenant_id=tenant_id)
@@ -111,3 +138,87 @@ def update_device_group(*, actor, tenant_id, device_group_id, name=None, descrip
     device_group.save()
     return device_group
 
+
+def update_tag(*, actor, tenant_id, tag_id, name=None, description=None):
+    require_write_tenant(actor, tenant_id)
+    tag = Tag.objects.get(id=tag_id, tenant_id=tenant_id)
+    if name is not None:
+        tag.name = name
+    if description is not None:
+        tag.description = description
+    tag.save()
+    return tag
+
+
+def update_interface(*, actor, tenant_id, interface_id, name=None, description=None, type=None, VRF=None):
+    require_write_tenant(actor, tenant_id)
+    interface = Interface.objects.get(id=interface_id, tenant_id=tenant_id)
+    if name is not None:
+        interface.name = name
+    if description is not None:
+        interface.description = description
+    if type is not None:
+        interface.type = type
+    if VRF is not None:
+        interface.VRF = VRF
+    interface.save()
+    return interface
+
+
+def update_filter(*, actor, tenant_id, filter_id, name=None, description=None):
+    require_write_tenant(actor, tenant_id)
+    filter = Filter.objects.get(id=filter_id, tenant_id=tenant_id)
+    if name is not None:
+        filter.name = name
+    if description is not None:
+        filter.description = description
+    filter.save()
+    return filter
+
+
+def update_filter_interface(
+    *, actor, tenant_id, filter_interface_id, direction=None, policy_sequence=None, enable=None
+):
+    require_write_tenant(actor, tenant_id)
+    filter_interface = FilterInterface.objects.get(id=filter_interface_id, tenant_id=tenant_id)
+    if direction is not None:
+        filter_interface.direction = direction
+    if policy_sequence is not None:
+        filter_interface.policy_sequence = policy_sequence
+    if enable is not None:
+        filter_interface.enable = enable
+    filter_interface.save()
+    return filter_interface
+
+
+def update_rule(
+    *,
+    actor,
+    tenant_id,
+    rule_id,
+    name=None,
+    description=None,
+    action=None,
+    log_type=None,
+    hit_count=None,
+    changed_by=None,
+    direction=None,
+):
+    require_write_tenant(actor, tenant_id)
+    rule = Rule.objects.get(id=rule_id, tenant_id=tenant_id)
+    if name is not None:
+        rule.name = name
+    if description is not None:
+        rule.description = description
+    if action is not None:
+        rule.action = action
+    if log_type is not None:
+        rule.log_type = log_type
+    if hit_count is not None:
+        rule.hit_count = hit_count
+    if changed_by is not None:
+        rule.changed_by = changed_by
+    if direction is not None:
+        rule.direction = direction
+    rule.save()
+    return rule
