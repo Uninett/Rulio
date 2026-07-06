5127a438815722f773c13d89bf1028bdbd244935
Ari Maman <ari.maman@sikt.no>
2026-07-06 14:35:58 +0200

    Add atomicity to the creation of rule, so that it cannot be interferred

--- src/backend/services/filter_objects/create_filter_objects.py
+++ src/backend/services/filter_objects/create_filter_objects.py
@@ -7,6 +7,7 @@ from backend.objects.filters.rule import Rule
 from backend.objects.filters.versionControl import VersionControl
 from backend.utils.logger import set_up_logger
 from backend.services.helper_user_tenant import get_tenant_by_id, require_write_tenant
+from django.db import transaction, IntegrityError
 
 
 logger = set_up_logger(__name__)
@@ -27,30 +28,35 @@ def create_rule(
     require_write_tenant(actor, tenant_id)
     tenant = get_tenant_by_id(tenant_id)
     now = datetime.now(timezone.utc)
-    rule = Rule(
-        name=name,
-        description=description,
-        filter=filter,
-        tenant=tenant,
-        action=action,
-        enable=enable,
-        log_type=log_type,
-        hit_count=hit_count,
-        date_created=now,
-        date_changed=now,
-        created_by=actor.id,
-        changed_by=actor.id,
-    )
     try:
-        rule.full_clean()
-    except DjangoValidationError as e:
-        logger.warning(f"Rule validation failed: {e.message_dict}")
-        raise ValueError(e.message_dict) from e
+        with transaction.atomic():
+            rule = Rule(
+                name=name,
+                description=description,
+                filter=filter,
+                tenant=tenant,
+                action=action,
+                enable=enable,
+                log_type=log_type,
+                hit_count=hit_count,
+                date_created=now,
+                date_changed=now,
+                created_by=actor.id,
+                changed_by=actor.id,
+            )
+            try:
+                rule.full_clean()
+            except DjangoValidationError as e:
+                logger.warning(f"Rule validation failed: {e.message_dict}")
+                raise ValueError(e.message_dict) from e
 
-    rule.save()
-    update_rule_sequence(actor=actor, tenant_id=tenant_id, rule=rule, new_sequence=rule.rule_sequence)
-    logger.info(f"Created {rule} for tenant={rule.tenant_id}")
-    return rule
+            rule.save()
+            update_rule_sequence(actor=actor, tenant_id=tenant_id, rule=rule, new_sequence=rule.rule_sequence)
+            logger.info(f"Created {rule} for tenant={rule.tenant_id}")
+            return rule
+    except IntegrityError as e:
+        logger.warning(f"Rule creation failed due to integrity error: {e}")
+        raise ValueError("Rule creation failed due to integrity error.") from e
 
 
 def get_or_create_rule(
