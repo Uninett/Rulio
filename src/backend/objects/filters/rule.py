11f20ee06669c25e4e20012be5d28e4b92063fd0
Ari Maman <ari.maman@sikt.no>
2026-07-06 14:16:56 +0200

    Refactor the rule object to make rule_filter redundant

--- src/backend/objects/filters/rule.py
+++ src/backend/objects/filters/rule.py
@@ -1,25 +1,27 @@
 from django.db import models
 
 from backend.objects.attributes.mixin.taggable_mixin import TaggableMixin
-from constants import DIRECTION_CHOICES_DJANGO
+from backend.objects.filters.filter import Filter
+from backend.objects.tenant_objects.tenant import Tenant
 
 
 class Rule(TaggableMixin, models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    tenant = models.ForeignKey("Tenant", on_delete=models.CASCADE)
-    action = models.CharField(max_length=255)
-    log_type = models.CharField(max_length=255)
-    hit_count = models.IntegerField(default=0)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    filter = models.ForeignKey(Filter, on_delete=models.CASCADE, related_name="rules")
+    tenant = models.ForeignKey(
+        Tenant, on_delete=models.CASCADE, related_name="rules"
+    )
+    action = models.CharField(max_length=255, blank=True, null=True)
+    enable = models.BooleanField(default=True)
+    rule_sequence = models.PositiveIntegerField(default=0)
+    log_type = models.CharField(max_length=255, blank=True, null=True)
+    hit_count = models.PositiveIntegerField(default=0)
     date_created = models.DateTimeField(auto_now_add=True)
     date_changed = models.DateTimeField(auto_now=True)
-    created_by = models.IntegerField()
-    changed_by = models.IntegerField()
-    direction = models.CharField(max_length=255, choices=DIRECTION_CHOICES_DJANGO, default="source")
+    created_by = models.CharField(max_length=255, blank=True, null=True)
+    changed_by = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return f"Rule(id={self.id}, name='{self.name}', description='{self.description}', tenant_id={self.tenant_id}, action='{self.action}', log_type='{self.log_type}', hit_count={self.hit_count}, date_created='{self.date_created}', date_changed='{self.date_changed}', created_by={self.created_by}, changed_by={self.changed_by})"
+        return f"Rule(id={self.id}, name='{self.name}', description='{self.description}', filter_id={self.filter_id}, tenant_id={self.tenant_id}, action='{self.action}', enable={self.enable}, rule_sequence={self.rule_sequence}, log_type='{self.log_type}', hit_count={self.hit_count}, date_created='{self.date_created}', date_changed='{self.date_changed}', created_by='{self.created_by}', changed_by='{self.changed_by}')"
 
-    def increment_hit_count(self):
-        self.hit_count += 1
-        self.save(update_fields=["hit_count"])
