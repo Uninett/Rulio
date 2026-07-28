b557ab249574dfa54c24a962a53a7830aa00d95a
Noah Johannes Borch <noah.borch.1@gmail.com>
2026-07-28 11:01:18 +0200

    Added uniqueness constraint to address groups

--- src/backend/objects/attributes/address_group_member.py
+++ src/backend/objects/attributes/address_group_member.py
@@ -5,5 +5,11 @@ class AddressGroupMember(models.Model):
     group = models.ForeignKey("AddressGroup", on_delete=models.CASCADE)
     address = models.ForeignKey("Address", on_delete=models.CASCADE)
 
+
     def __str__(self):
         return f"AddressGroupMember(group_id={self.group_id}, address_id={self.address_id})"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["group", "address"], name="unique_group_address")
+        ]
