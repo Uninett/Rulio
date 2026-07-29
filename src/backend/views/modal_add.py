134e93f04f74cc0568a84bbd1f6d239439ac3e73
Ari Maman <ari.maman@sikt.no>
2026-07-29 12:43:42 +0200

    Add functionality for making rules

--- src/backend/views/modal_add.py
+++ src/backend/views/modal_add.py
@@ -71,10 +71,8 @@ def get_add_modal_config(object_type):
             "supports_types": False,
             "form_partial": "partials/modals/_rule_form.html",
             "post_url": reverse("post-rule-view"),
-            "target": "#rules-table",
-            "swap": "beforeend",
-            "refresh_url": reverse("rules-page") + f"?filter_id={{filter_id}}&filter_name={{filter_name}}",
-            "modal_refresh_target": "#rules-content",
+            "target": "#modal-container",
+            "swap": "innerHTML",
         },
         "addresses": {
             "title": "Add Address",
@@ -144,7 +142,6 @@ def get_add_modal_config(object_type):
 def get_add_modal(request, object_type):
     config = get_add_modal_config(object_type)
 
-    # Check if modal supports multiple types
     if config.get("supports_types"):
         selected_type = config["default_type"]
         modal_content_partial = config["types"][selected_type]
@@ -155,6 +152,11 @@ def get_add_modal(request, object_type):
     modal_post_url = config.get("post_url")
     if config.get("supports_types") and config.get("post_urls"):
         modal_post_url = config["post_urls"].get(selected_type)
+
+    object_data = {}
+
+    if object_type == "rules":
+        object_data["filter_id"] = request.GET.get("filter_id", "")
 
     context = {
         "modal_title": config["title"],
@@ -171,7 +173,7 @@ def get_add_modal(request, object_type):
         "modal_submit_handler": config.get("submit_handler"),
         "modal_refresh_url": config.get("refresh_url"),
         "modal_refresh_target": config.get("modal_refresh_target"),
-        "object_data": {},
+        "object_data": object_data,
         "selected_group_ids": [],
         "selected_address_ids": [],
         "selected_service_ids": [],
@@ -179,7 +181,6 @@ def get_add_modal(request, object_type):
         "object_tags": [],
     }
 
-    # If object_type is address, service or device, then show all groups
     if object_type in ["addresses", "services"]:
         context["group_options"] = get_group_options_view(request, object_type)
         context["item_options"] = get_item_options_view(request, object_type)
@@ -187,7 +188,7 @@ def get_add_modal(request, object_type):
     if object_type == "users":
         context["tenant_options"] = [
             {"id": tenant.id, "name": tenant.tenant_name}
-            for tenant in Tenant.objects.exclude(id=GLOBAL_TENANT_ID).order_by("tenant_name")
+            for tenant in Tenant.objects.exclude(id=1).order_by("tenant_name")
         ]
 
     if object_type == "tenants":
